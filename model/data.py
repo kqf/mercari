e87c7c8df5ea8e35d8d4546321e5dd592c1e5bb4
kqf <q.dev.null@gmail.com>
2017-12-20 18:54:52 +0100

    Clean the data selection part

--- model/data.py
+++ model/data.py
@@ -1,16 +1,29 @@
+import pandas as pd
 from sklearn.base import TransformerMixin, BaseEstimator, clone
 from sklearn.base import BaseEstimator, TransformerMixin
+
+
 
 # Create a class to select numerical or categorical columns 
 class DataFrameSelector(BaseEstimator, TransformerMixin):
     def __init__(self, attribute_names):
         self.attribute_names = attribute_names
 
+    def transform(self, X):
+        return X[self.attribute_names].to_dict(orient='records')
+
     def fit(self, X, y=None):
         return self
 
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
     def transform(self, X):
         return X[self.attribute_names].values
+
+    def fit(self, X, y=None):
+        return self
 
 class SelectColumnsTransfomer(BaseEstimator, TransformerMixin):
 
@@ -19,88 +32,37 @@ class SelectColumnsTransfomer(BaseEstimator, TransformerMixin):
 
     def transform(self, X, **transform_params):
         trans = X[self.columns].copy() 
-        return trans
+        return trans    
 
     def fit(self, X, y=None, **fit_params):
         return self
-    
+
 
 class DataFrameFunctionTransformer(BaseEstimator, TransformerMixin):
     
-    def __init__(self, func, impute = False):
+    def __init__(self, func):
         self.func = func
-        self.impute = impute
-        self.series = pd.Series() 
 
     def transform(self, X, **transformparams):
-        if self.impute:
-            return pd.DataFrame(X).fillna(self.series).copy()
         return pd.DataFrame(X).apply(self.func).copy()
 
     def fit(self, X, y=None, **fitparams):
-        """ Fixes the values to impute or does nothing
-        
-        Parameters
-        ----------
-        X : pandas DataFrame
-        y : not used, API requirement
-                
-        Returns
-        ----------
-        self  
-        """
-        
-        if self.impute:
-            self.series = pd.DataFrame(X).apply(self.func).copy()
         return self
     
     
 class DataFrameFeatureUnion(BaseEstimator, TransformerMixin):
-    """ A DataFrame transformer that unites several DataFrame transformers
-    
-    Fit several DataFrame transformers and provides a concatenated
-    Data Frame
-    
-    Parameters
-    ----------
-    list_of_transformers : list of DataFrameTransformers
-        
-    """ 
+
     def __init__(self, list_of_transformers):
         self.list_of_transformers = list_of_transformers
         
     def transform(self, X, **transformparamn):
-        """ Applies the fitted transformers on a DataFrame
-        
-        Parameters
-        ----------
-        X : pandas DataFrame
-        
-        Returns
-        ----------
-        concatted :  pandas DataFrame
-        
-        """
-        
         concatted = pd.concat([transformer.transform(X)
                             for transformer in
-                            self.fitted_transformers_], axis=1).copy()
+                            self.fitted_transformers_], join='inner', axis=1).copy()
         return concatted
 
 
     def fit(self, X, y=None, **fitparams):
-        """ Fits several DataFrame Transformers
-        
-        Parameters
-        ----------
-        X : pandas DataFrame
-        y : not used, API requirement
-        
-        Returns
-        ----------
-        self : object
-        """
-        
         self.fitted_transformers_ = []
         for transformer in self.list_of_transformers:
             fitted_trans = clone(transformer).fit(X, y=None, **fitparams)
@@ -109,30 +71,10 @@ class DataFrameFeatureUnion(BaseEstimator, TransformerMixin):
     
 
 class ToDummiesTransformer(BaseEstimator, TransformerMixin):
-    """ A Dataframe transformer that provide dummy variable encoding
-    """
     
     def transform(self, X, **transformparams):
-        """ Returns a dummy variable encoded version of a DataFrame
-        
-        Parameters
-        ----------
-        X : pandas DataFrame
-        
-        Returns
-        ----------
-        trans : pandas DataFrame
-        
-        """
-    
         trans = pd.get_dummies(X).copy()
         return trans
 
     def fit(self, X, y=None, **fitparams):
-        """ Do nothing operation
-        
-        Returns
-        ----------
-        self : object
-        """
         return self
--- test/test_data.py
+++ test/test_data.py
@@ -1,21 +1,42 @@
 import unittest
 import pandas as pd
-from model.data import SelectColumnsTransfomer, DataFrameSelector
+from sklearn.pipeline import Pipeline, make_pipeline
+from model.data import SelectColumnsTransfomer, DataFrameSelector, ToDummiesTransformer, DataFrameFunctionTransformer, DataFrameFeatureUnion
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, LabelBinarizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 
 
 class ExploreData(unittest.TestCase):
 
-	def test_column_transfer(self):
-		df = pd.read_csv("input/train.tsv", sep='\t')
-		selector = SelectColumnsTransfomer(['item_condition_id'])
-		print(selector.fit(df).transform(df))
+	# def test_column_transfer(self):
+	# 	df = pd.read_csv("input/train.tsv", sep='\t')
+	# 	selector = SelectColumnsTransfomer(['item_condition_id'])
+	# 	print(selector.fit(df).transform(df))
 
 
-	def test_dataframe_selector(self):
+	def test_data_content(self):
 		df = pd.read_csv("input/train.tsv", sep='\t')
-		selector = DataFrameSelector(['item_condition_id'])
-		print(selector.fit(df).transform(df))
+
+		condition = make_pipeline(
+			SelectColumnsTransfomer(["item_condition_id"]),
+			ToDummiesTransformer()
+		)
+
+		brand = make_pipeline(
+			SelectColumnsTransfomer(["brand_name"]),
+			DataFrameFunctionTransformer(lambda x: x.astype("category")),
+			DataFrameFunctionTransformer(lambda x: x.dropna())
+		)
+
+		union = DataFrameFeatureUnion([brand, condition])
+		print(union.fit_transform(df))
+
+
+
+
+
+
 
 
 
