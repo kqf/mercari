0d023f5b5537d741060c327c8bf3f072cea7e3b1
Sasha <q.dev.null@gmail.com>
2018-03-22 16:43:26 +0100

    Configure travis CI for further tests

--- test/test_data.py
+++ test/test_data.py
@@ -1,42 +1,9 @@
-import unittest
+import pytest
 import pandas as pd
-from sklearn.pipeline import make_pipeline, FeatureUnion
-from model.data import SelectColumnsTransfomer, DataFrameSelector, DataFrameFunctionTransformer, CategoricalSelector
-from sklearn.preprocessing import StandardScaler
-from sklearn.feature_extraction import DictVectorizer
+from model.model import evaluate
 
 
-
-class ExploreData(unittest.TestCase):
-
-	def test_data_content(self):
-		df = pd.read_csv("input/train.tsv", sep='\t').dropna()
-
-		condition = make_pipeline(
-			DataFrameSelector(["item_condition_id"]),
-			StandardScaler()
-		)
-
-		brand = make_pipeline(
-			SelectColumnsTransfomer(["brand_name"]),
-			DataFrameFunctionTransformer(lambda x: x.astype("category")),
-			# DataFrameFunctionTransformer(lambda x: x.dropna()),
-			CategoricalSelector(["brand_name"]),
-			DictVectorizer()
-		)
-
-		union = FeatureUnion([
-			('brand', brand),
-			('condition', condition)
-		])
-		print(union.fit_transform(df))
-
-
-
-
-
-
-
-
-
-
+@pytest.mark.parametrize("sample_size", (200, 400, 1000))
+def test_data_content(sample_size):
+    train = pd.read_table("data/train.tsv")
+    evaluate(train.sample(sample_size))
